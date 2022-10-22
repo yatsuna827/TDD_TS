e98ced6aad9f4467a595fc45bfe8f4511fd15ab4
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-10-22 22:10:48 +0900

    plusとexpressionの解決を実装

--- src/money.test.ts
+++ src/money.test.ts
@@ -1,4 +1,4 @@
-import { doller, franc, times, plus } from './money'
+import { doller, franc, times, plus, reduce } from './money'
 
 describe('Money', () => {
   it('test multiplication', () => {
@@ -13,7 +13,13 @@ describe('Money', () => {
   })
   it('test simple addition', () => {
     const sum = plus(doller(5), doller(5))
-    expect(sum).toEqual(doller(10))
+    const reduced = reduce(sum, 'USD')
+    expect(reduced).toEqual(doller(10))
+  })
+  it('test multiple addition', () => {
+    const sum = plus(doller(5), doller(6), doller(7))
+    const reduced = reduce(sum, 'USD')
+    expect(reduced).toEqual(doller(18))
   })
   it('equality', () => {
     expect(doller(5)).toEqual(doller(5))
--- src/money.ts
+++ src/money.ts
@@ -3,10 +3,36 @@ type Currency = 'USD' | 'CHF'
 type Money<T extends Currency> = {
   amount: number
   currency: T
+  __tag: 'Money'
 }
 
-const money = <T extends Currency>(amount: number, currency: T): Money<T> => ({ amount, currency })
+type Sum = {
+  augend: Expression
+  addend: Expression
+  __tag: 'Sum'
+}
+
+type Expression = Money<Currency> | Sum
+
+const money = <T extends Currency>(amount: number, currency: T): Money<T> => ({ amount, currency, __tag: 'Money' })
+const sum = (augend: Expression, addend: Expression): Sum => ({ augend, addend, __tag: 'Sum' })
 export const doller = (amount: number): Money<'USD'> => money(amount, 'USD')
 export const franc = (amount: number): Money<'CHF'> => money(amount, 'CHF')
 export const times = <T extends Currency>({ amount, currency }: Money<T>, multiplier: number): Money<T> => money(amount * multiplier, currency)
-export const plus = (augend: Money<any>, addend: Money<any>): Money<any> => money(augend.amount + addend.amount, augend.currency)
+export const plus = (first: Expression, ...rest: [Expression, ...Expression[]]): Expression => {
+  const [second, ...r] = rest
+  const s = sum(first, second)
+  return r.length == 0 ? s : plus(s, ...(r as [Expression, ...Expression[]])) // これはTSの推論力不足を補うための仕方のないas
+}
+
+export const reduce = <T extends Currency>(exp: Expression, currency: T): Money<T> => {
+  if (exp.__tag == 'Money') {
+    return { ...exp, currency }
+  } else {
+    const { augend, addend } = exp
+    const { amount: a } = reduce(augend, currency)
+    const { amount: b } = reduce(addend, currency)
+
+    return money(a + b, currency)
+  }
+}
