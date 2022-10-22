7b771ba5e0727809abebc9519c4879f391361c5f
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-10-22 19:30:21 +0900

    Money型に通貨単位型引数を追加し、ドルとフランを区別できるように

--- src/money.ts
+++ src/money.ts
@@ -1,7 +1,10 @@
-type Money = {
+type Currency = 'USD' | 'CHF'
+
+type Money<T extends Currency> = {
   amount: number
+  currency: T
 }
 
-export const doller = (amount: number): Money => ({ amount })
-export const franc = (amount: number): Money => ({ amount })
-export const times = (money: Money, multiplier: number): Money => doller(money.amount * multiplier)
+export const doller = (amount: number): Money<'USD'> => ({ amount, currency: 'USD' })
+export const franc = (amount: number): Money<'CHF'> => ({ amount, currency: 'CHF' })
+export const times = <T extends Currency>(money: Money<T>, multiplier: number): Money<T> => ({ amount: money.amount * multiplier, currency: money.currency })
