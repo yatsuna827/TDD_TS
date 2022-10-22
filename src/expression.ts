e291ab85f8dcf44a12f2ad02bce40ca9ed1589e0
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-10-23 00:08:32 +0900

    timesの実装と型定義を改良

--- src/expression.ts
+++ src/expression.ts
@@ -9,7 +9,18 @@ export type Sum = {
 export type Expression = Money<Currency> | Sum
 
 const sum = (augend: Expression, addend: Expression): Sum => ({ augend, addend, __tag: 'Sum' })
-export const times = <T extends Currency>({ amount, currency }: Money<T>, multiplier: number): Money<T> => money(amount * multiplier, currency)
+
+type MultRet<E extends Expression> = E extends Money<infer T> ? Money<T> : Sum
+export const times = <E extends Expression>(exp: E, multiplier: number): MultRet<E> => {
+  if (exp.__tag === 'Money') {
+    const { amount, currency } = exp
+    return money(amount * multiplier, currency) as MultRet<E>
+  } else {
+    const { augend, addend } = exp
+    return sum(times(augend, multiplier), times(addend, multiplier)) as MultRet<E>
+  }
+}
+
 export const plus = (first: Expression, ...rest: [Expression, ...Expression[]]): Expression => {
   const [second, ...r] = rest
   const s = sum(first, second)
