ef02a65b9cf08cb2cb4d3e7c78b17fcc48f363fa
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-10-22 19:16:12 +0900

    equality

--- src/money.test.ts
+++ src/money.test.ts
@@ -3,7 +3,10 @@ import { doller, times } from './money'
 describe('Money', () => {
   it('test multiplication', () => {
     const five = doller(5)
-    const ten = times(five, 2)
-    expect(ten).toEqual(doller(10))
+    expect(times(five, 2)).toEqual(doller(10))
+    expect(times(five, 3)).toEqual(doller(15))
+  })
+  it('equality', () => {
+    expect(doller(5)).toEqual(doller(5))
   })
 })
